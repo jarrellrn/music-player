f811883dea5c1f17d03c1f44f5ab14b84ff2261f
jarrellrn <jarrellrn@gmail.com>
2021-12-05 13:39:57 -0600

    added func to update progress bar

--- script.js
+++ script.js
@@ -64,6 +64,12 @@ function nextSong() {
   playSong()
 }
 
+function updateProgress(e) {
+  const {duration, currentTime} = e.srcElement
+  const progressPercent = (currentTime / duration) * 100
+  progress.style.width = `${progressPercent}%`
+}
+
 
 // Event listeners
 playBtn.addEventListener("click", () => {
@@ -79,3 +85,5 @@ playBtn.addEventListener("click", () => {
 // Change song events
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+
+audio.addEventListener("timeupdate", updateProgress)
